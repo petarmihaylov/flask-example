3686ec0ae6604e6fb531b2789d02ab91d088410d
petarmihaylov <petarmihaylov@users.noreply.github.com>
2014-08-11 13:30:05 -0400

    Added more Flask extensions to install_requires
    Added the followqing Flask Extensions: 'Flask-MongoAlchemy>=0.6.1', 'flask-script', 'flask-bootstrap', 'flask-moment', 'flask-wtf', 'flask-sqlalchemy', 'flask-migrate', 'flask-mail', 'flask-login', 'forgerypy', 'flask-pagedown', 'markdown', 'bleach', 'flask-httpauth'

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='YourAppName',
       author='Your Name',
       author_email='example@example.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.10.1', 'MarkupSafe'],
+      install_requires=['Flask>=0.10.1', 'MarkupSafe', 'Flask-MongoAlchemy>=0.6.1', 'flask-script', 'flask-bootstrap', 'flask-moment', 'flask-wtf', 'flask-sqlalchemy', 'flask-migrate', 'flask-mail', 'flask-login', 'forgerypy', 'flask-pagedown', 'markdown', 'bleach', 'flask-httpauth'],
      )
